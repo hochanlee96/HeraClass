dc847a35adff038c13d4bcff3c530955a3532eb3
HochanLee <j65hcl@gmail.com>
2020-09-25 15:46:41 +0900

    added logout component, connected it to dropdown logout item

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './containers/Home/Home';
 import Layout from './components/hoc/Layout/Layout';
 import ClassList from './containers/ClassList/ClassList';
 import ClassDetail from './containers/ClassDetail/ClassDetail';
+import Logout from './components/Auth/Logout/Logout';
 import * as authActions from './store/actions/auth';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
       <Route path="/home" exact component={Home} />
       <Route path="/detail/:classId" component={ClassDetail} />
       <Route path="/class-list" exact component={ClassList} />
+      <Route path="/logout" exact component={Logout} />
       <Route path="/" exact component={Home} />
       <Redirect to="/home" />
     </Switch>)
---
+++ src/components/Auth/Logout/Logout.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import * as authActions from '../../../store/actions/auth';
+
+const Logout = props => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(authActions.logout());
+    }, [dispatch]);
+
+    return <Redirect to='/home' />;
+}
+
+
+export default Logout;
